c878808a5eb2f4f7dd762fc4673661b1fefaf01c
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2017-11-05 15:12:24 +0100

    created game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,15 @@
 class GamesController < ApplicationController
+  include GamesHelper
+
   def game
+    @grid = generate_grid(10)
   end
 
   def score
+    @end = Time.now
+    @word = params[:word]
+    @start = DateTime.parse(params[:start])
+    @grid = params[:grid].split("")
+    @result = run_game(@word, @grid, @start, @end)
   end
 end
--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,34 @@
 module GamesHelper
+  def hell
+    "hello"
+  end
 end
+
+require 'open-uri'
+require 'json'
+
+def generate_grid(grid_size)
+  # TODO: generate random grid of letters
+  a = []
+  grid_size.times{ a << (65 + rand(25)).chr }
+  a
+end
+
+def run_game(attempt, grid, start_time, end_time)
+  # TODO: runs the game and return detailed hash of result
+  url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
+  duration = (end_time - start_time).to_i
+  h = { time: duration, score: 0, message: "Lost! Not in the grid" }
+  attempt = attempt.upcase.split("")
+  if attempt & grid == attempt
+    d = JSON.parse(open(url).read)
+    if d["found"]
+      h[:score] = d["length"].to_i * 20 - duration * 3
+      h[:message] = "Got it! Well done!"
+    else
+      h[:message] = "Lost! Not an english word!"
+    end
+  end
+  h
+end
+
